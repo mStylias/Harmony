69a4c50e590dee8dae1a36f8e414e05ca57bd254
Michalis Stylianidis <57811193+mStylias@users.noreply.github.com>
2024-04-03 16:04:02 +0300

    Add cancellation token to validator

--- src/Harmony.Cqrs/Validators/IHarmonyOperationValidator.cs
+++ src/Harmony.Cqrs/Validators/IHarmonyOperationValidator.cs
@@ -4,5 +4,5 @@ namespace Harmony.Cqrs.Validators;
 
 public interface IHarmonyOperationValidator<in TOperation, TOutput> where TOperation : IHarmonyOperation
 {
-    public Task<TOutput> ValidateAsync(TOperation operation);
+    public Task<TOutput> ValidateAsync(TOperation operation, CancellationToken cancellation = default);
 }
