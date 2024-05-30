2eb5fd536288c7602c9da0d6589bbb138f2364ff
StyleM <57811193+mStylias@users.noreply.github.com>
2024-05-30 22:48:18 +0300

    Remove Success with Metada (not planned) (LIB) #5

--- src/Library/Harmony.Results/Success.cs
+++ src/Library/Harmony.Results/Success.cs
@@ -28,38 +28,3 @@ public class Success
         _logAction?.Invoke();
     }
 }
-
-public class Success<TMetadata>
-{
-    public string? Message { get; init; }
-    public TMetadata Metadata { get; init; }
-    private readonly Action? _logAction;
-
-    public Success(TMetadata metadata)
-    {
-        Metadata = metadata;
-    }
-    public Success(Action logAction, TMetadata metadata)
-    {
-        _logAction = logAction;
-        Metadata = metadata;
-    }
-    
-    public Success(string message, TMetadata metadata)
-    {
-        Message = message;
-        Metadata = metadata;
-    }
-
-    public Success(string message, Action logAction, TMetadata metadata)
-    {
-        Message = message;
-        Metadata = metadata;
-        _logAction = logAction;
-    }
-    
-    public void Log()
-    {
-        _logAction?.Invoke();
-    }
-}
