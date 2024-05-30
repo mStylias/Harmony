8b850b54c05cea04a007202ae6443d41b06a06e9
StyleM <57811193+mStylias@users.noreply.github.com>
2024-05-30 23:51:32 +0300

    Fix bugs (LIB) #3

--- src/Library/Harmony.Results/Abstractions/ILoggableHarmonyError.cs
+++ src/Library/Harmony.Results/Abstractions/ILoggableHarmonyError.cs
@@ -3,14 +3,15 @@ using Microsoft.Extensions.Logging;
 
 namespace Harmony.Results.Abstractions;
 
-public interface ILoggableHarmonyError : IHarmonyError
+public interface ILoggableHarmonyError<out TError> : IHarmonyError
+    where TError : class, ILoggableHarmonyError<TError>
 {
-    ILoggableHarmonyError InitializeLogMessage(ILogger logger, LogLevel logLevel);
-    ILoggableHarmonyError InitializeLogMessage(ILogger logger, LogLevel logLevel, EventId eventId);
-    ILoggableHarmonyError SetLogException(Exception exception);
-    ILoggableHarmonyError AppendLogMessage([StructuredMessageTemplate] string message);
-    ILoggableHarmonyError AppendLogMessage([StructuredMessageTemplate] string message, params object[] args);
-    ILoggableHarmonyError PrependLogMessage([StructuredMessageTemplate] string message);
-    ILoggableHarmonyError PrependLogMessage([StructuredMessageTemplate] string message, params object[] args);
+    TError InitializeLogMessage(ILogger logger, LogLevel logLevel);
+    TError InitializeLogMessage(ILogger logger, LogLevel logLevel, EventId eventId);
+    TError SetLogException(Exception exception);
+    TError AppendLogMessage([StructuredMessageTemplate] string message);
+    TError AppendLogMessage([StructuredMessageTemplate] string message, params object[] args);
+    TError PrependLogMessage([StructuredMessageTemplate] string message);
+    TError PrependLogMessage([StructuredMessageTemplate] string message, params object[] args);
     void Log();
 }
