6b30dae2654851de132a4259657fdcff96888e77
StyleM <57811193+mStylias@users.noreply.github.com>
2024-06-21 23:34:24 +0300

    Create parent class for Query and Command #11 (LIB)

---
+++ src/Library/Harmony.Cqrs/HarmonyOperation.cs
@@ -0,0 +1,71 @@
+﻿using Harmony.Cqrs.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Harmony.Cqrs;
+
+public abstract class HarmonyOperation<TInput, TOutput> : IHarmonyOperationWithIO<TInput, TOutput>
+{
+    public abstract TInput? Input { get; set; }
+    public virtual TOutput Execute(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task<TOutput> ExecuteAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public IServiceScope? Scope { get; set; }
+    public void Dispose()
+    {
+        if (Scope is null) return;
+        
+        Scope.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
+
+public abstract class HarmonyOperation<TOutput> : IHarmonyOperation
+{
+    public virtual TOutput Execute(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task<TOutput> ExecuteAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public IServiceScope? Scope { get; set; }
+    public void Dispose()
+    {
+        if (Scope is null) return;
+        
+        Scope.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
+
+public abstract class HarmonyOperation : IHarmonyOperation
+{
+    public virtual void Execute(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task ExecuteAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IServiceScope? Scope { get; set; }
+    public void Dispose()
+    {
+        if (Scope is null) return;
+        
+        Scope.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
