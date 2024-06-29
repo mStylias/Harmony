26a1ddaa53d718038d23640a02a11755380a1148
StyleM <57811193+mStylias@users.noreply.github.com>
2024-06-29 20:09:41 +0300

    IHarmonyOperationValidator -> IOperationValidator

--- src/Library/Harmony.Cqrs/Validators/IOperationValidator.cs
+++ src/Library/Harmony.Cqrs/Validators/IOperationValidator.cs
@@ -2,7 +2,7 @@
 
 namespace Harmony.Cqrs.Validators;
 
-public interface IHarmonyOperationValidator<in TOperation, TOutput> where TOperation : IHarmonyOperation
+public interface IOperationValidator<in TOperation, TOutput> where TOperation : IHarmonyOperation
 {
     public Task<TOutput> ValidateAsync(TOperation operation, CancellationToken cancellation = default);
 }
